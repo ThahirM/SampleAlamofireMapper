f7d4b1717e7d7d641c3d285a0b36f15c6e9fdac2
Thahir Maheen <thahir@farabi.ae>
2017-09-28 11:42:22 +0400

    rename Search to Tracks

--- SampleAlamofireMapper/SampleAlamofireMapper/TracksViewController.swift
+++ SampleAlamofireMapper/SampleAlamofireMapper/TracksViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController {
+class TracksViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
